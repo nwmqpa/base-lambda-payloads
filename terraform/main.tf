31b04b11c4e74718789b310e17c582069c0ca887
Thomas Nicollet <thomas.nicollet@nebulis.io>
2022-11-03 17:34:44 +0100

    :pencil: Fix several typos

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 locals {
   tag_url   = "https://api.github.com/repos/${var.package_repository}/tags"
-  files_url = var.packages_version == "latest" ? "https://github.com/${var.package_repository}/releases/latest/download/lambda_payload_${var.runtime}_${var.architecture}.zip" : "https://github.com/${var.package_repository}/releases/download/${var.version}/lambda_payload_${var.runtime}_${var.architecture}.zip"
+  files_url = var.packages_version == "latest" ? "https://github.com/${var.package_repository}/releases/latest/download/lambda_payload_${var.runtime}_${var.architecture}.zip" : "https://github.com/${var.package_repository}/releases/download/${var.packages_version}/lambda_payload_${var.runtime}_${var.architecture}.zip"
 }
 
 data "http" "this" {
@@ -13,7 +13,7 @@ resource "null_resource" "this" {
   }
 
   provisioner "local-exec" {
-    command = "curl -L ${local.url} -o ${path.module}/lambda_payload_${var.runtime}_${var.architecture}.zip"
+    command = "curl -L ${local.files_url} -o ${path.module}/lambda_payload_${var.runtime}_${var.architecture}.zip"
   }
 }
 
